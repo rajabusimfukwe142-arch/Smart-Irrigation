1b738558003b0506b2bdb60ad96749855ceabad8
rajabusimfukwe142-arch <rajabusimfukwe142@gmail.com>
2026-06-27 19:44:48 +0300

    Update login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,13 +1,18 @@
 export default function handler(req, res) {
+  console.log("BODY RECEIVED:", req.body);
+
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
 
   const { email, password } = req.body;
 
+  console.log("EMAIL:", email);
+  console.log("PASSWORD:", password);
+
   if (email === "test@gmail.com" && password === "1234") {
     return res.status(200).json({ message: "Login successful" });
   }
 
-  return res.status(401).json({ message: "Invalid credentials" });
+  return res.status(401).json({ message: "Neno la siri si sahihi" });
 }
